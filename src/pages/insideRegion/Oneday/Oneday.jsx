4e1dd4cce9ec8f90120109690913e462622b73ee
sarin punk <sarin.bhattarai@icp.edu.np>
2023-04-28 10:14:20 +0545

    feat(ineffable pics)

--- src/pages/insideRegion/Oneday/Oneday.jsx
+++ src/pages/insideRegion/Oneday/Oneday.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import SV from "../../../resources/assets/sv.jpg";
+import SV2 from "../../../resources/assets/sv2.jpg";
+import SV3 from "../../../resources/assets/sv3.jpg";
+import PS from "../../../resources/assets/ps.jpg";
+import PS2 from "../../../resources/assets/ps2.webp";
+import PS3 from "../../../resources/assets/ps3.jpg";
+import AB from "../../../resources/assets/ab.jpg";
+import AB2 from "../../../resources/assets/ab2.jpg";
+import AB3 from "../../../resources/assets/ab3.jpg";
+import PT from "../../../resources/assets/pt-1.jpg";
+import PT2 from "../../../resources/assets/pt-2.jpg";
+import PT3 from "../../../resources/assets/pt-3.jpg";
+import DH from "../../../resources/assets/dh.jpeg";
+import DH2 from "../../../resources/assets/dh2.jpg";
+import DH3 from "../../../resources/assets/dh3.jpg";
+import AT from "../../../resources/assets/at.jpeg";
 
 const Oneday = () => {
   return (
@@ -27,6 +42,318 @@ const Oneday = () => {
             </p>
             <div className="subregion-grid-img">
               <img src={SV} alt="a1" />
+              <img src={SV2} alt="a1" />
+              <img src={SV3} alt="a1" />
+            </div>
+
+            <div className="region-row">
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Package Booking
+                </h3>
+                <h5>1. It includes all the necessary permits.</h5>
+                <h5>2. Expert guide during the trip.</h5>
+                <h5>
+                  3. Three meals( breakfast, Lunch & dinner) during the trek.
+                </h5>
+                <h5>
+                  4. Accomodation with free Wifi and Hot Shower during the trek.
+                </h5>
+                <h5>5. Transportation Service fees.</h5>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Non Package Booking
+                </h3>
+
+                <h5>
+                  It includes only Expert guide from our side. Other all costs
+                  should be bared from your side.
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="subregion-grid">
+            <h1
+              style={{
+                textAlign: "center",
+                fontSize: "42px",
+              }}
+            >
+              Pokhara Sight Seeing
+            </h1>
+            <p>
+              It includes visiting famous world peace pagoda and huge Shiva
+              statue with outstading view of the entire fewa lake with crystal
+              like shiny mountains adding scent in the gold fewa lake all will
+              be seen in one picture.
+            </p>
+            <div className="subregion-grid-img">
+              <img src={PS} alt="a1" />
+              <img src={PS2} alt="a1" />
+              <img src={PS3} alt="a1" />
+            </div>
+
+            <div className="region-row">
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Package Booking
+                </h3>
+                <h5>1. It includes all the necessary permits.</h5>
+                <h5>2. Expert guide during the trip.</h5>
+                <h5>
+                  3. Three meals( breakfast, Lunch & dinner) during the trek.
+                </h5>
+                <h5>
+                  4. Accomodation with free Wifi and Hot Shower during the trek.
+                </h5>
+                <h5>5. Transportation Service fees.</h5>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Non Package Booking
+                </h3>
+
+                <h5>
+                  It includes only Expert guide from our side. Other all costs
+                  should be bared from your side.
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="subregion-grid">
+            <h1
+              style={{
+                textAlign: "center",
+                fontSize: "42px",
+              }}
+            >
+              Australian Base Camp
+            </h1>
+            <p>
+              Australian Camp is a picturesque viewpoint in the Kaski district
+              of Nepal, close to the city of Pokhara. It was named after
+              Australian trekking groups who first discovered the area. The
+              viewpoint is located at an altitude of 2,050 meters (6,726 feet)
+              and provides stunning views of the Annapurna and Dhaulagiri
+              mountain ranges.
+            </p>
+            <div className="subregion-grid-img">
+              <img src={AB} alt="a1" />
+              <img src={AB2} alt="a1" />
+              <img src={AB3} alt="a1" />
+            </div>
+
+            <div className="region-row">
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Package Booking
+                </h3>
+                <h5>1. It includes all the necessary permits.</h5>
+                <h5>2. Expert guide during the trip.</h5>
+                <h5>
+                  3. Three meals( breakfast, Lunch & dinner) during the trek.
+                </h5>
+                <h5>
+                  4. Accomodation with free Wifi and Hot Shower during the trek.
+                </h5>
+                <h5>5. Transportation Service fees.</h5>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Non Package Booking
+                </h3>
+
+                <h5>
+                  It includes only Expert guide from our side. Other all costs
+                  should be bared from your side.
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="subregion-grid">
+            <h1
+              style={{
+                textAlign: "center",
+                fontSize: "42px",
+              }}
+            >
+              Panchase Trek
+            </h1>
+            <p>
+              The Panchase Trek is a lesser-known but beautiful trek located in
+              the western region of Nepal. It takes trekkers through lush
+              forests, picturesque villages, and terraced fields, providing
+              breathtaking views of the Himalayan peaks, including Dhaulagiri,
+              Annapurna, and Manaslu. The trek starts in Pokhara and passes
+              through the villages of Bumdi, Panchase Bhanjyang, and Bhadaure.
+              Along the way, trekkers can interact with the friendly Gurung
+              people, experiencing the local culture firsthand. The highlight of
+              the trek is reaching the summit of Panchase Hill, offering
+              panoramic views of the surrounding mountains and valleys.
+            </p>
+            <div className="subregion-grid-img">
+              <img src={PT} alt="a1" />
+              <img src={PT2} alt="a1" />
+              <img src={PT3} alt="a1" />
+            </div>
+
+            <div className="region-row">
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Package Booking
+                </h3>
+                <h5>1. It includes all the necessary permits.</h5>
+                <h5>2. Expert guide during the trip.</h5>
+                <h5>
+                  3. Three meals( breakfast, Lunch & dinner) during the trek.
+                </h5>
+                <h5>
+                  4. Accomodation with free Wifi and Hot Shower during the trek.
+                </h5>
+                <h5>5. Transportation Service fees.</h5>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Non Package Booking
+                </h3>
+
+                <h5>
+                  It includes only Expert guide from our side. Other all costs
+                  should be bared from your side.
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="subregion-grid">
+            <h1
+              style={{
+                textAlign: "center",
+                fontSize: "42px",
+              }}
+            >
+              Dhampus
+            </h1>
+            <p>
+              Dhampus is a small village located in the Kaski district of Nepal,
+              around 18 kilometers (11 miles) from Pokhara. At an altitude of
+              1,700 meters (5,577 feet), it is well-known for its breathtaking
+              views of the Annapurna and Machhapuchhre (Fishtail) mountain
+              ranges.
+            </p>
+            <div className="subregion-grid-img">
+              <img src={DH} alt="a1" />
+              <img src={DH2} alt="a1" />
+              <img src={DH3} alt="a1" />
+            </div>
+
+            <div className="region-row">
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Package Booking
+                </h3>
+                <h5>1. It includes all the necessary permits.</h5>
+                <h5>2. Expert guide during the trip.</h5>
+                <h5>
+                  3. Three meals( breakfast, Lunch & dinner) during the trek.
+                </h5>
+                <h5>
+                  4. Accomodation with free Wifi and Hot Shower during the trek.
+                </h5>
+                <h5>5. Transportation Service fees.</h5>
+              </div>
+              <div>
+                <h3
+                  style={{
+                    fontSize: "28px",
+                    fontWeight: "400",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Non Package Booking
+                </h3>
+
+                <h5>
+                  It includes only Expert guide from our side. Other all costs
+                  should be bared from your side.
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div className="subregion-grid">
+            <h1
+              style={{
+                textAlign: "center",
+                fontSize: "42px",
+              }}
+            >
+              Astham
+            </h1>
+            <p>
+              Astham is a picturesque village located in the Kaski district of
+              Nepal, approximately 20 kilometers (12 miles) away from Pokhara.
+              The village is situated at an altitude of 1,500 meters (4,921
+              feet) and boasts stunning views of the Annapurna and Manaslu
+              mountain ranges.
+            </p>
+            <div className="subregion-grid-img">
+              <img src={AT} alt="a1" />
+              <img src={AT} alt="a1" />
+              <img src={AT} alt="a1" />
             </div>
 
             <div className="region-row">
